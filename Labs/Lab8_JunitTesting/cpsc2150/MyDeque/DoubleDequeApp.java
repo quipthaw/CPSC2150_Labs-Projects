4827bbf2955675553fd9d18eaea7c22377cedb88
Manning Graham <98988760+quipthaw@users.noreply.github.com>
2023-02-07 23:22:07 -0500

    Add files via upload

---
+++ Labs/Lab8_JunitTesting/cpsc2150/MyDeque/DoubleDequeApp.java
@@ -0,0 +1,139 @@
+/*
+CPSC 2150
+Manning Graham and Harrison Tun
+ */
+package cpsc2150.MyDeque;
+import java.util.Scanner;
+public class DoubleDequeApp<T> {
+    public static void main(String[] args) {
+
+        IDeque <Double> q;
+        String a = "3";
+        String b = "1";
+
+        //Prompt  the  user  to  pick  an  implementation  using  the  following
+        //message:"Enter  1  for  array  implementation  or  2  for  List implementation"
+
+        while (!a.equals("1") && !a.equals("2"))
+        {
+            Scanner myObj = new Scanner(System.in);
+            //Scanner myObj2 = new Scanner(System.in);
+            System.out.println("Enter 1 for array implementation or 2 for List implementation");
+            a = myObj.nextLine();
+        }
+        if (a == "1"){ q = new ArrayDeque<Double>(); }
+        else q = new ListDeque<Double>();
+
+        while(b!= "12")
+        {
+
+            Scanner myObj2 = new Scanner(System.in);
+            System.out.println("i.Add to the end of the deque\n" +
+                    "ii.Add to the front of the deque\n" +
+                    "iii.Remove from the front of the deque\n" +
+                    "iv.Remove from the end of the deque\n" +
+                    "v.Peek at the front of the deque\n" +
+                    "vi.Peek at the end of the deque\n" +
+                    "vii.Insert into a position in the deque\n" +
+                    "viii.Remove a value from any position in the deque and return it\n" +
+                    "ix.Peek at a value in any position in the deque\n" +
+                    "x.Returns the length of the deque\n" +
+                    "xi.Clears the deque\n" +
+                    "xii.Exit" );
+            System.out.println(q.toString());
+            b = myObj2.nextLine();
+            if (b.equals("1"))
+            {
+                Scanner myObj3 = new Scanner(System.in);
+                System.out.println("What integer would you like to add?");
+                b = myObj3.nextLine();
+                q.enqueue(Double.parseDouble(b));
+                b = "1";
+            }
+
+            else if(b.equals("2"))
+            {
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("What integer would you like to add?");
+                b = scanner.nextLine();
+                q.inject(Double.parseDouble(b));
+                b = "2";
+            }
+
+            else if(b.equals("3"))
+            {
+                Double x = q.dequeue();
+                System.out.println("You removed " + x);
+            }
+            else if(b.equals("4"))
+            {
+                Double x = q.removeLast();
+                System.out.println("You removed " + x);
+            }
+
+            else if(b.equals("5"))
+            {
+                Double tmp = q.peek();
+                System.out.println(tmp);
+            }
+            else if(b.equals("6"))
+            {
+                Double x = q.endOfDeque();
+                System.out.println(x + " is at the end of the deque");
+            }
+            else if(b.equals("7"))
+            {
+                Scanner myObj3 = new Scanner(System.in);
+                System.out.println("What integer would you like to add?");
+                b = myObj3.nextLine();
+                System.out.println("What position would you like to add at?(First element is 1)");
+                String pos = myObj3.nextLine();
+                int test = Integer.parseInt(pos);
+                if(test>q.length() ||  test<1) { System.out.println("Invalid input"); }
+                else { q.insert(Double.parseDouble(b),Integer.parseInt(pos)); }
+                b = "7";
+            }
+            else if(b.equals("8"))
+            {
+                Scanner myObj3 = new Scanner(System.in);
+                System.out.println("What position would you like to remove at?(First element is 1)");
+                b = myObj3.nextLine();
+                Double test = Double.parseDouble(b);
+                if(test>q.length() ||  test<1) { System.out.println("Invalid input"); }
+
+                else
+                {
+                    Double x = q.remove(Integer.parseInt(b));
+                    System.out.println("You removed " + x);
+                }
+                b = "8";
+            }
+            else if(b.equals("9"))
+            {
+                Scanner myObj3 = new Scanner(System.in);
+                System.out.println("What position would you like to peek at?(First element is 1)");
+                b = myObj3.nextLine();
+                Double test = Double.parseDouble(b);
+
+                if(test>q.length() ||  test<1) { System.out.println("Invalid input"); }
+
+                else { System.out.println("That element is " + q.get(Integer.parseInt(b))); }
+                b = "9";
+            }
+            else if(b.equals("10")){ System.out.println(q.length()); }
+
+            else if(b.equals("11")){
+                q.clear();
+                System.out.println(" Deque Cleared. ");
+            }
+
+            else if(b.equals("12")){ b = "12"; }
+
+            else if(b.equals("13")){ System.out.println (q.toString()); }
+
+            else System.out.println("You have not Entered a valid menu option. Please try again");
+
+            q.toString();
+        }
+    }
+}
---
+++ Labs/Lab8_JunitTesting/cpsc2150/MyDeque/TestArrayDeque.java
@@ -0,0 +1,436 @@
+package cpsc2150.MyDeque;
+
+import cpsc2150.MyDeque.ArrayDeque;
+import cpsc2150.MyDeque.IDeque;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+
+
+public class TestArrayDeque {
+
+    private IDeque<Integer> MakeADeque()
+    {
+        IDeque<Integer> q;
+        q = new ArrayDeque<Integer>();
+        return q;
+    }
+
+
+
+
+
+
+    //Tests for enqueue
+    @Test
+    public void testEnqueue1Element()
+    {
+        IDeque<Integer> q = MakeADeque();
+        q.enqueue(0);
+        assertTrue(q.toString().equals("<0>"));
+    }
+
+    @Test
+    public void testEnqueue5Element()
+    {
+        IDeque<Integer> q = MakeADeque();
+
+
+        q.enqueue(0);
+        q.enqueue(1);
+        q.enqueue(2);
+        q.enqueue(3);
+        q.enqueue(4);
+        assertTrue(q.toString().equals("<0, 1, 2, 3, 4>"));
+    }
+
+    @Test
+    public void testEnqueue101Element()
+    {
+        IDeque<Integer> q = MakeADeque();
+        String test = "<";
+        for(int i=0;i<101;i++) {
+            q.enqueue(i);
+            if(i<99)
+                test += i + ", ";
+            else if(i==99)
+                test += i;
+        }
+        test += ">";
+        assertTrue(q.length()==100);
+        assertTrue(q.toString().equals(test));
+    }
+
+
+
+
+
+
+    // Tests for Deque
+    @Test
+    public void TestDequeue1Deque()
+    {
+        IDeque<Integer> q = MakeADeque();
+        q.enqueue(0);
+        q.enqueue(1);
+        q.dequeue();
+        assertTrue(q.toString().equals("<1>"));
+    }
+
+    @Test
+    public void TestDequeue1Element()
+    {
+        IDeque<Integer> q = MakeADeque();
+        q.enqueue(1);
+        q.dequeue();
+        assertTrue(q.toString().equals("<>"));
+    }
+
+    @Test
+    public void TestDequeueMultipleDequeues()
+    {
+        IDeque<Integer> q = MakeADeque();
+        q.enqueue(0);
+        q.enqueue(1);
+        q.enqueue(2);
+        q.enqueue(3);
+        q.enqueue(4);
+        q.dequeue();
+        q.dequeue();
+        assertTrue(q.toString().equals("<2, 3, 4>"));
+    }
+
+
+
+
+
+
+
+
+
+    // Tests for inject
+    @Test
+    public void TestInject1Element()
+    {
+        IDeque<Integer> q = MakeADeque();
+        q.inject(0);
+        assertTrue(q.toString().equals("<0>"));
+    }
+
+    @Test
+    public void TestInject5Elements()
+    {
+        IDeque<Integer> q = MakeADeque();
+        q.inject(0);
+        q.inject(1);
+        q.inject(2);
+        q.inject(3);
+        q.inject(4);
+        assertTrue(q.toString().equals("<4, 3, 2, 1, 0>"));
+    }
+
+    @Test
+    public void ThirdTestInject100Elements()
+    {
+
+        IDeque<Integer> q = MakeADeque();
+        String test = "<";
+        for(int i= 99;i>=0;i--) {
+            if (i > 0)
+                test += i + ", ";
+            else if (i == 0)
+                test += i;
+        }
+        test += ">";
+
+        for(int i= 0;i<100;i++) {
+            q.inject(i);
+        }
+
+
+        System.out.println("test: "+ test + " \nq: " +q.toString());
+
+        assertTrue(q.toString().equals(test));
+    }
+
+
+
+
+
+
+
+
+
+
+    // Tests for removeLast
+    @Test
+    public void TestRemoveLast1Element()
+    {
+        IDeque<Integer> q = MakeADeque();
+        q.enqueue(0);
+        assertTrue(q.removeLast()==0);
+        assertTrue(q.toString().equals("<>"));
+    }
+
+    @Test
+    public void TestRemoveLast3Elements()
+    {
+        IDeque<Integer> q = MakeADeque();
+        q.enqueue(0);
+        q.enqueue(1);
+        q.enqueue(2);
+        assertTrue(q.removeLast()==2);
+        assertTrue(q.toString().equals("<0, 1>"));
+    }
+
+    @Test
+    public void TestRemoveLast100Elements()
+    {
+        IDeque<Integer> q = MakeADeque();
+        String test = "<";
+        for(int i=0;i<100;i++) {
+            q.enqueue(i);
+            if(i<98)
+                test += i + ", ";
+            else if(i==98)
+                test += i + ">";
+        }
+        q.removeLast();
+        assertTrue(q.toString().equals(test));
+        assertTrue(q.length()==99);
+    }
+
+
+
+
+
+
+
+    // Tests for Clear
+    @Test
+    public void FirstTestClearMultipleElements()
+    {
+        IDeque<Integer> q = MakeADeque();
+        q.enqueue(0);
+        q.enqueue(1);
+        q.enqueue(2);
+        q.clear();
+        assertTrue(q.toString().equals("<>"));
+    }
+
+    @Test
+    public void SecondTestClear1Element()
+    {
+        IDeque<Integer> q = MakeADeque();
+        q.clear();
+        assertTrue(q.toString().equals("<>"));
+    }
+
+    @Test
+    public void TestClearFull()
+    {
+        IDeque<Integer> q = MakeADeque();
+        for(int i=0;i<100;i++)
+            q.enqueue(i);
+        q.clear();
+        assertTrue(q.toString().equals("<>"));
+    }
+
+
+
+
+
+
+
+    // Tests for peek
+    @Test
+    public void FirstTestPeek1Element()
+    {
+        IDeque<Integer> q = MakeADeque();
+        q.enqueue(0);
+        assertTrue(q.peek() == 0);
+    }
+
+    @Test
+    public void TestPeekMultipleElements()
+    {
+        IDeque<Integer> q = MakeADeque();
+        q.enqueue(0);
+        q.enqueue(1);
+        q.enqueue(2);
+        assertTrue(q.peek()==0);
+    }
+
+    @Test
+    public void TestPeekFullArray() {
+
+        IDeque<Integer> q = MakeADeque();
+        for (int i = 0; i < 100; i++)
+            q.enqueue(i);
+
+        assertTrue(q.peek()==0);
+    }
+
+
+
+
+
+
+
+
+
+
+
+    // Tests for endOfDeque
+    @Test
+    public void FirstTestEndOfDeque1Element()
+    {
+        IDeque<Integer> q = MakeADeque();
+        q.enqueue(0);
+        assertTrue(q.endOfDeque()==0);
+    }
+
+    @Test
+    public void SecondTestEndOfDequeMultipleElements()
+    {
+        IDeque<Integer> q = MakeADeque();
+        q.enqueue(0);
+        q.enqueue(1);
+        q.enqueue(2);
+        assertTrue(q.endOfDeque()==2);
+    }
+
+    @Test
+    public void TestEndOfDequeFullDeque()
+    {
+        IDeque<Integer> q = MakeADeque();
+        for(int i=0;i<100;i++)
+            q.enqueue(i);
+        assertTrue(q.endOfDeque()==99);
+    }
+
+
+
+
+
+
+
+
+
+    // Tests for insert
+    @Test
+    public void TestInsertAtFront()
+    {
+        IDeque<Integer> q = MakeADeque();
+        q.enqueue(0);
+        q.enqueue(2);
+        q.insert(1,1);
+        assertTrue(q.toString().equals("<1, 0, 2>"));
+    }
+
+    @Test
+    public void TestInsertAtEnd()
+    {
+        IDeque<Integer> q = MakeADeque();
+        q.enqueue(0);
+        q.enqueue(1);
+        q.insert(2,3);
+        System.out.println(q.toString());
+        assertTrue(q.toString().equals("<0, 1>"));
+    }
+
+    @Test
+    public void ThirdTestInsertInMiddle()
+    {
+        IDeque<Integer> q = MakeADeque();
+
+        q.enqueue(0);
+        q.enqueue(1);
+        q.enqueue(2);
+        q.enqueue(3);
+
+        q.insert(8,3);
+        assertTrue(q.toString().equals("<0, 1, 8, 2, 3>"));
+    }
+
+
+
+
+
+
+
+
+
+    // Tests for remove
+    @Test
+    public void TestRemoveAtFront()
+    {
+        IDeque<Integer> q = MakeADeque();
+        q.enqueue(0);
+        assertTrue(q.remove(1)==0);
+        assertTrue(q.toString().equals("<>"));
+    }
+
+    @Test
+    public void SecondTestRemoveAtEnd()
+    {
+        IDeque<Integer> q = MakeADeque();
+        q.enqueue(0);
+        q.enqueue(1);
+        q.enqueue(2);
+        assertTrue(q.remove(3)==2);
+        assertTrue(q.toString().equals("<0, 1>"));
+    }
+
+    @Test
+    public void ThirdTestRemoveFromMiddle()
+    {
+        IDeque<Integer> q = MakeADeque();
+        q.enqueue(0);
+        q.enqueue(1);
+        q.enqueue(2);
+        assertTrue(q.remove(2)==1);
+        assertTrue(q.toString().equals("<0, 2>"));
+    }
+
+
+
+
+
+
+
+
+
+    // Tests for get
+    @Test
+    public void FirstTestGetFront()
+    {
+        IDeque<Integer> q = MakeADeque();
+        q.inject(0);
+        assertTrue(q.get(1)==0);
+    }
+
+    @Test
+    public void SecondTestGetLast()
+    {
+        IDeque<Integer> q = MakeADeque();
+        q.enqueue(0);
+        q.enqueue(1);
+        q.enqueue(3);
+
+        assertTrue(q.get(3) == 3);
+    }
+
+    @Test
+    public void ThirdTestGetMiddle()
+    {
+        IDeque<Integer> q = MakeADeque();
+        q.enqueue(0);
+        q.enqueue(1);
+        q.enqueue(2);
+        assertTrue(q.get(2)==1);
+    }
+
+
+}
+
